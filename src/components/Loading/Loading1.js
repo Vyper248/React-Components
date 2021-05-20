86d082240ed52ebc4aabb7a388f30a4599f50b9b
Vyper248 <vyper375@hotmail.com>
2021-05-20 17:36:09 +0100

    Added Loading1.

---
+++ src/components/Loading/Loading1.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledComp = styled.div`
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border-top: 5px solid ${props => props.color};
+    border-left: 5px solid ${props => props.color};
+    animation-name: spin;
+    animation-duration: 0.7s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+
+    @keyframes spin {
+        from {transform: rotate(0deg);}
+        to {transform: rotate(360deg);}
+    }
+`;
+
+const Loading1 = ({color='black'}) => {
+    return (
+        <StyledComp color={color}></StyledComp>
+    );
+}
+
+export default Loading1;
