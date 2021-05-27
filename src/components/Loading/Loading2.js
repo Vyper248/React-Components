abe52565858915cd9e927c5a15b25595389e7b9e
Vyper248 <vyper375@hotmail.com>
2021-05-27 12:49:15 +0100

    Made adjustments to Loading2 to make it easier to vary the number of segments being used and animation delay.

--- src/components/Loading/Loading2.js
+++ src/components/Loading/Loading2.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+let arr = new Array(10).fill(1).map((a,i) => i+1).reverse();
+
 const StyledComp = styled.div`
     display: inline-block;
     margin: 5px;
@@ -10,21 +12,14 @@ const StyledComp = styled.div`
         background-color: ${props => props.color};
         ${props => props.smooth === 2 ? 'border-radius: 5px': null};
         ${props => props.smooth === 3 ? 'border-radius: 50%': null};
-        width: 60px;
-        height: 5px;
+        width: 50px;
+        height: ${Math.floor((50/arr.length)-1)+'px'};
         margin-bottom: 1px;
         transform-origin: ${props => props.origin};
         animation: anim1 0.8s ease-in infinite alternate;
     }
 
-    & > div:nth-child(8) { animation-delay: -0.2s; }
-    & > div:nth-child(7) { animation-delay: -0.4s; }
-    & > div:nth-child(6) { animation-delay: -0.6s; }
-    & > div:nth-child(5) { animation-delay: -0.8s; }
-    & > div:nth-child(4) { animation-delay: -1s; }
-    & > div:nth-child(3) { animation-delay: -1.2s; }
-    & > div:nth-child(2) { animation-delay: -1.4s; }
-    & > div:nth-child(1) { animation-delay: -1.6s; }
+    ${arr.map((a,i) => `& > div:nth-child(${a}) { animation-delay: -${(1/(arr.length/8))*0.2*(i+1)}s; }`)}
 
     @keyframes anim1 {
         from {transform: scaleX(1);}
@@ -35,14 +30,7 @@ const StyledComp = styled.div`
 const Loading2 = ({color='black', rotation=90, smooth=2, origin='middle'}) => {
     return (
         <StyledComp color={color} rotation={rotation} smooth={parseInt(smooth)} origin={origin}>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+        { arr.map(a => <div key={'loading2-'+a}></div>) }
         </StyledComp>
     );
 }
